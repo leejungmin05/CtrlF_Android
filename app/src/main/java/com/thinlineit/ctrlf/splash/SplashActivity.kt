2c8ec6e7180fde13aafd3306362b4f42ab4de01f
leejungmin05 <74166576+leejungmin05@users.noreply.github.com>
2021-05-23 00:33:27 +0900

    remove main coroutineScope (+1 squashed commits)
    Squashed commits:
    
    [76d1913] fix delay time & add checkLogin function as TODO (+1 squashed commits)
    
    Squashed commits:
    
    [3302fb0] add splash activity

--- app/src/main/java/com/thinlineit/ctrlf/splash/SplashActivity.kt
+++ app/src/main/java/com/thinlineit/ctrlf/splash/SplashActivity.kt
@@ -1,29 +1,47 @@
 package com.thinlineit.ctrlf.splash
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.thinlineit.ctrlf.MainActivity
 import com.thinlineit.ctrlf.R
+import kotlinx.android.synthetic.main.activity_splash.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Glide
+            .with(this@SplashActivity)
+            .asGif()
+            .load(R.drawable.splash_gif)
+            .into(iv_gif_view)
+
         CoroutineScope(Dispatchers.Default).launch {
-            delay(1000L)
-            CoroutineScope(Dispatchers.Main).launch {
+            delay(1500L)
+            if (checkLogin()) {
                 this@SplashActivity.startActivity(
                     Intent(
                         this@SplashActivity,
                         MainActivity::class.java
                     )
                 )
+            } else {
+                // start loginActivity
             }
         }
+    }
+
+    private fun checkLogin(): Boolean {
+        //TODO: check if user have login via sharedPreference
+        return true
     }
 }
