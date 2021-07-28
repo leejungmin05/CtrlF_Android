813ac8eb261b018ea0c8b95186e3aff555bfc168
leejungmin05 <74166576+leejungmin05@users.noreply.github.com>
2021-07-28 17:12:19 +0900

    add Event class

--- app/src/main/java/com/thinlineit/ctrlf/registration/LoginActivity.kt
+++ app/src/main/java/com/thinlineit/ctrlf/registration/LoginActivity.kt
@@ -4,43 +4,42 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.thinlineit.ctrlf.MainActivity
 import com.thinlineit.ctrlf.R
 import com.thinlineit.ctrlf.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+    private val binding: ActivityLoginBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_login)
+    }
     private val viewModel: LoginViewModel by lazy {
         ViewModelProvider(this).get(LoginViewModel::class.java)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
-        viewModel.loginStatus.observe(this, Observer {
-            if (it) {
+
+        viewModel.loginStatus.observe(this) {
+            it.getContentIfNotHandled()?.let {
                 MainActivity.start(this)
                 finish()
-            } else {
-                Toast.makeText(this, ALERTTEXT, Toast.LENGTH_LONG).show()
             }
-        })
-
-        viewModel.eventClick.observe(this, {
-            RegisterActivity.start(this)
-            finish()
-        })
-
-        viewModel.alertLiveData.observe(this) {
-            Toast.makeText(this, it, Toast.LENGTH_LONG).show()
         }
+
+        viewModel.eventClick.observe(this) {
+            it.getContentIfNotHandled()?.let {
+                RegisterActivity.start(this)
+                finish()
+            }
+        }
+
     }
 
     companion object {
@@ -50,5 +49,5 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private val ALERTTEXT = "이메일과 비밀번호를 입력해주세요"
 }
+
--- app/src/main/java/com/thinlineit/ctrlf/registration/LoginViewModel.kt
+++ app/src/main/java/com/thinlineit/ctrlf/registration/LoginViewModel.kt
@@ -3,14 +3,19 @@ package com.thinlineit.ctrlf.registration
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.thinlineit.ctrlf.model.User
 import com.thinlineit.ctrlf.network.RegistrationService
 import com.thinlineit.ctrlf.util.Application
-import kotlinx.coroutines.CoroutineScope
+import com.thinlineit.ctrlf.util.Event
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.util.regex.Pattern
 
 class LoginViewModel : ViewModel() {
+    private val _loginStatus = MutableLiveData<Event<Boolean>>()
+    val loginStatus: LiveData<Event<Boolean>>
+        get() = _loginStatus
 
     val _email = MutableLiveData<String>()
     val email: LiveData<String>
@@ -20,48 +25,61 @@ class LoginViewModel : ViewModel() {
     val password: LiveData<String>
         get() = _password
 
-    private val _loginStatus = MutableLiveData<Boolean>()
-    val loginStatus: LiveData<Boolean>
-        get() = _loginStatus
+    val loginMessage = MutableLiveData<String>()
 
-    val eventClick: MutableLiveData<Boolean> = MutableLiveData()
-    val alertLiveData = MutableLiveData<String>()
+    private val _eventClick = MutableLiveData<Event<Boolean>>()
+    val eventClick: LiveData<Event<Boolean>>
+        get() = _eventClick
 
     init {
         _email.value = ""
         _password.value = ""
     }
 
-
-    fun loginChk() {
-        if (_email.value == "" || _password.value == "") {
-            _loginStatus.value = false
-        } else {
-            doLogin()
-        }
-    }
-
     private fun doLogin() {
-        CoroutineScope(Dispatchers.IO).launch {
+        viewModelScope.launch(Dispatchers.IO) {
             try {
                 val loginResponse = RegistrationService.USER_API.requestLogin(
                     User(email.value.toString(), password.value.toString())
                 )
                 val token = loginResponse.token
-                Application.prefs.setString("token", token)
-                Application.prefs.setString("email", _email.value.toString())
-                Application.prefs.setString("password", _password.value.toString())
-                _loginStatus.postValue(true)
+                Application.prefs.setString(TOKEN, token)
+                Application.prefs.setString(EMAIL, email.value.toString())
+                Application.prefs.setString(PASSWORD, password.value.toString())
+                _loginStatus.postValue(Event(true))
             } catch (e: Exception) {
-                alertLiveData.postValue(e.message)
-
+                loginMessage.postValue(e.message)
             }
         }
     }
 
-    fun startRegisterActivity() {
-        eventClick.value = true
+    private fun String.isEmailValid(): Boolean {
+        val expression = EMAILREGEX
+        val pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE)
+        val matcher = pattern.matcher(this)
+        return matcher.matches()
     }
+
+    fun startRegisterActivity() {
+        _eventClick.value = Event(true)
+    }
+
+    fun loginChk() {
+        if (email.value == "" || password.value == "") {
+            loginMessage.postValue(NULLMESSAGE)
+        } else if (!email.value.toString().isEmailValid()) {
+            loginMessage.postValue(EMAILMESSAGE)
+        } else {
+            doLogin()
+        }
+    }
+
+    private val NULLMESSAGE = "비밀번호와 아이디를 입력해주세요."
+    private val EMAILMESSAGE = "이메일 형식이 올바르지 않습니다."
+    private val TOKEN = "token"
+    private val EMAIL = "email"
+    private val PASSWORD = "password"
+    private val EMAILREGEX = "^[\\w.-]+@([\\w\\-]+\\.)+[A-Z]{2,8}$"
 
 
 }
