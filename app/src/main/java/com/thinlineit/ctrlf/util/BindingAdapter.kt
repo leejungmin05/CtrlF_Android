91b7654191f0c74616a6c067c9224e6b52dd8ffe
leejungmin05 <74166576+leejungmin05@users.noreply.github.com>
2021-07-28 17:14:31 +0900

    fix data binding adapter & layout

--- app/src/main/java/com/thinlineit/ctrlf/util/BindingAdapter.kt
+++ app/src/main/java/com/thinlineit/ctrlf/util/BindingAdapter.kt
@@ -1,5 +1,7 @@
 package com.thinlineit.ctrlf.util
 
+import android.view.View
+import android.widget.EditText
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 
@@ -9,3 +11,11 @@ fun <T> setRecyclerViewData(recyclerView: RecyclerView, data: T) {
     if (recyclerView.adapter is BindingRecyclerViewAdapter<*>)
         (recyclerView.adapter as BindingRecyclerViewAdapter<T>).setData(data)
 }
+
+@BindingAdapter("app:onFocusLost")
+fun EditText.onFocusLost(callback: () -> Unit) {
+    setOnFocusChangeListener { v, hasFocus ->
+        if(!hasFocus ) callback.invoke()
+    }
+}
+
