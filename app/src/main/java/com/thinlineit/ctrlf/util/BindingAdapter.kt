b5099b49b2c2a161e09365adff7e944de667ee1d
leejungmin05 <74166576+leejungmin05@users.noreply.github.com>
2021-07-29 22:26:55 +0900

    resolve commit conflict

--- app/src/main/java/com/thinlineit/ctrlf/util/BindingAdapter.kt
+++ app/src/main/java/com/thinlineit/ctrlf/util/BindingAdapter.kt
@@ -1,11 +1,9 @@
 package com.thinlineit.ctrlf.util
 
-import android.view.View
 import android.widget.EditText
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.mukesh.MarkdownView
-
 
 @BindingAdapter("app:data")
 fun <T> setRecyclerViewData(recyclerView: RecyclerView, data: T) {
@@ -13,7 +11,6 @@ fun <T> setRecyclerViewData(recyclerView: RecyclerView, data: T) {
         (recyclerView.adapter as BindingRecyclerViewAdapter<T>).setData(data)
 }
 
-<<<<<<< HEAD:app/src/main/java/com/thinlineit/ctrlf/util/BindingAdapter.kt
 @BindingAdapter("app:onFocusLost")
 fun EditText.onFocusLost(callback: () -> Unit) {
     setOnFocusChangeListener { v, hasFocus ->
@@ -21,11 +18,9 @@ fun EditText.onFocusLost(callback: () -> Unit) {
     }
 }
 
-=======
 @BindingAdapter("markdownString")
 fun setMarkdownString(markdownView: MarkdownView, string: String?) {
     string?.let {
         markdownView.setMarkDownText(it)
     }
 }
->>>>>>> dev:app/src/main/java/com/thinlineit/ctrlf/util/BidingAdapter.kt
