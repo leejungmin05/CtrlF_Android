caed682adb687dde2fdd280638a9c1c40c615111
leejungmin05 <74166576+leejungmin05@users.noreply.github.com>
2021-08-15 01:44:26 +0900

    split into userRepository

--- app/src/main/java/com/thinlineit/ctrlf/util/BindingAdapter.kt
+++ app/src/main/java/com/thinlineit/ctrlf/util/BindingAdapter.kt
@@ -1,9 +1,14 @@
 package com.thinlineit.ctrlf.util
 
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.EditText
 import androidx.databinding.BindingAdapter
+import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView
 import com.mukesh.MarkdownView
+import com.thinlineit.ctrlf.registration.RegisterViewModel
+
 
 @BindingAdapter("app:data")
 fun <T> setRecyclerViewData(recyclerView: RecyclerView, data: T) {
@@ -24,4 +29,21 @@ fun setMarkdownString(markdownView: MarkdownView, string: String?) {
     string?.let {
         markdownView.setMarkDownText(it)
     }
+}
+
+@BindingAdapter("app:addTextChangeListener")
+fun addTextChangeListener(view: EditText, viewModel: ViewModel) {
+    view.addTextChangedListener(object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            when (viewModel) {
+                is RegisterViewModel -> viewModel.checkPasswordSame()
+            }
+        }
+    })
 }
