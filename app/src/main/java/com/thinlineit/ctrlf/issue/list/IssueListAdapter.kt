007da65a72238926d63c1864298a3b3468bf7009
Donggi Lee <rkdmf1026@naver.com>
2021-08-17 14:29:32 +0900

    Refactor layout
    main fragment의 리사이클러뷰 레이아웃을 dp로 맞춤

--- app/src/main/java/com/thinlineit/ctrlf/issue/list/IssueListAdapter.kt
+++ app/src/main/java/com/thinlineit/ctrlf/issue/list/IssueListAdapter.kt
@@ -8,6 +8,7 @@ import com.thinlineit.ctrlf.R
 import com.thinlineit.ctrlf.databinding.ListItemIssueBinding
 import com.thinlineit.ctrlf.issue.IssueDao
 import com.thinlineit.ctrlf.util.BindingRecyclerViewAdapter
+import com.thinlineit.ctrlf.util.setBackground
 
 class IssueListAdapter(private val clickListener: (IssueDao) -> Unit) :
     RecyclerView.Adapter<IssueListAdapter.ViewHolder>(),
@@ -21,15 +22,24 @@ class IssueListAdapter(private val clickListener: (IssueDao) -> Unit) :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val issueDao = issueList[position]
-        holder.bind(issueDao, clickListener)
+        holder.bind(issueDao, clickListener,position)
     }
 
     class ViewHolder(private val dataBinding: ListItemIssueBinding) :
         RecyclerView.ViewHolder(dataBinding.root) {
-        fun bind(issueDao: IssueDao, clickListener: (IssueDao) -> Unit) {
-            dataBinding.issue = issueDao
-            dataBinding.root.setOnClickListener {
-                clickListener(issueDao)
+        fun bind(issueDao: IssueDao, clickListener: (IssueDao) -> Unit, position: Int) {
+            var resourceId: Int
+            when (position % 3) {
+                1 -> resourceId = R.drawable.ic_issue_2
+                2 -> resourceId = R.drawable.ic_issue_3
+                else -> resourceId = R.drawable.ic_issue_1
+            }
+            dataBinding.apply {
+                issueItem.setBackground(resourceId)
+                issue = issueDao
+                root.setOnClickListener {
+                    clickListener(issueDao)
+                }
             }
         }
 
