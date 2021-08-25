f3379dc38d1f6d00c39849aa76315959f0a8af15
Donggi Lee <rkdmf1026@naver.com>
2021-08-25 19:34:54 +0900

    Fix conflict

--- app/src/main/java/com/thinlineit/ctrlf/util/BindingAdapter.kt
+++ app/src/main/java/com/thinlineit/ctrlf/util/BindingAdapter.kt
@@ -7,35 +7,21 @@ import android.widget.EditText
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
 import com.mukesh.MarkdownView
 import com.thinlineit.ctrlf.registration.RegistrationViewModel
 
-
 @BindingAdapter("app:data")
 fun <T> setRecyclerViewData(recyclerView: RecyclerView, data: T) {
-<<<<<<< HEAD
-    if (recyclerView.adapter is BindingRecyclerViewAdapter<*> && data != null){
-            (recyclerView.adapter as BindingRecyclerViewAdapter<T>).setData(data)
+    if (recyclerView.adapter is BindingRecyclerViewAdapter<*> && data != null) {
+        (recyclerView.adapter as BindingRecyclerViewAdapter<T>).setData(data)
     }
 }
 
 @BindingAdapter("app:fragmentData")
 fun <T> setFragmentStateData(viewPager: ViewPager2, data: T) {
-    if(viewPager.adapter is BindingFragmentStateAdapter<*> && data != null)
+    if (viewPager.adapter is BindingFragmentStateAdapter<*> && data != null)
         (viewPager.adapter as BindingFragmentStateAdapter<T>).setData(data)
-}
-
-@BindingAdapter("app:onFocusLost")
-fun EditText.onFocusLost(callback: () -> Unit) {
-    setOnFocusChangeListener { v, hasFocus ->
-        if(!hasFocus ) callback.invoke()
-=======
-    if (recyclerView.adapter is BindingRecyclerViewAdapter<*> && data != null) {
-        (recyclerView.adapter as BindingRecyclerViewAdapter<T>).setData(data)
->>>>>>> dev
-    }
 }
 
 @BindingAdapter("markdownString")
