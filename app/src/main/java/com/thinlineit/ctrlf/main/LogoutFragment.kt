97229f44fd299f37b6984378469e41a517dca562
Donggi Lee <rkdmf1026@naver.com>
2021-08-29 16:57:33 +0900

    Fix review
    deleteInfo 함수 접근지정자 설정

--- app/src/main/java/com/thinlineit/ctrlf/main/LogoutFragment.kt
+++ app/src/main/java/com/thinlineit/ctrlf/main/LogoutFragment.kt
@@ -2,12 +2,12 @@ package com.thinlineit.ctrlf.main
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.thinlineit.ctrlf.R
 import com.thinlineit.ctrlf.databinding.FragmentLogoutBinding
 import com.thinlineit.ctrlf.splash.SplashActivity
@@ -16,29 +16,34 @@ import com.thinlineit.ctrlf.util.Application
 class LogoutFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = (DataBindingUtil.inflate(
-            inflater,
-            R.layout.fragment_logout,
-            container,
-            false
-        ) as FragmentLogoutBinding).apply {
-            logoutBtn.setOnClickListener {
-                deleteInfo()
-                (activity as AppCompatActivity).finishAffinity()
-                val intent = Intent(activity, SplashActivity::class.java)
-                startActivity(intent)
+        val binding =
+            (
+                DataBindingUtil.inflate(
+                    inflater,
+                    R.layout.fragment_logout,
+                    container,
+                    false
+                )
+                    as FragmentLogoutBinding
+                ).apply {
+                logoutBtn.setOnClickListener {
+                    deleteInfo()
+                    (activity as AppCompatActivity).finishAffinity()
+                    val intent = Intent(activity, SplashActivity::class.java)
+                    startActivity(intent)
+                }
+                cancelBtn.setOnClickListener {
+                    (activity as AppCompatActivity).onBackPressed()
+                }
             }
-            cancelBtn.setOnClickListener {
-                (activity as AppCompatActivity).onBackPressed()
-            }
-        }
         return binding.root
     }
 
-    fun deleteInfo() {
+    private fun deleteInfo() {
         Application.preferenceUtil.setString(EMAIL, "")
         Application.preferenceUtil.setString(PASSWORD, "")
         Application.preferenceUtil.setString(TOKEN, "")
