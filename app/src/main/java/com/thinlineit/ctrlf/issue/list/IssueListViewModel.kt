64a825e49cab4f491134cb3268b816e21db43214
yunkwon-shin <shin.yunkwon@linecorp.com>
2021-09-01 23:59:14 +0900

    Clean up mock createIssue Logic

--- app/src/main/java/com/thinlineit/ctrlf/issue/list/IssueListViewModel.kt
+++ app/src/main/java/com/thinlineit/ctrlf/issue/list/IssueListViewModel.kt
@@ -19,17 +19,13 @@ class IssueListViewModel : ViewModel() {
         _issueList.value = createIssue()
     }
 
-    private fun createIssue(): MutableList<IssueDao> {
-        val imsiList: MutableList<IssueDao> = arrayListOf()
-        var contentStr =
-            "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
-        for (i in 1..9) {
-            if (i % 2 != 0) {
-                imsiList.add(IssueDao(i, "title$i", 1, 1, "2021-07-12", contentStr))
-            } else {
-                imsiList.add(IssueDao(i, "title$i", 1, 1, "2021-07-12", contentStr + contentStr))
-            }
+    private fun createIssue(): List<IssueDao> {
+        val contentStr =
+            "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz" +
+                "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+        return (1..9).map { i ->
+            if (i % 2 != 0) IssueDao(i, "title$i", 1, 1, "2021-07-12", contentStr)
+            else IssueDao(i, "title$i", 1, 1, "2021-07-12", contentStr + contentStr)
         }
-        return imsiList
     }
 }
