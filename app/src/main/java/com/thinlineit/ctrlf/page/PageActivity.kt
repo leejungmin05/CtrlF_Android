3b2c6a1ae16ccc77dd51706b1bc461a7683a8eae
minki_hong <gaogai2@naver.com>
2021-09-23 10:59:53 +0900

    check ktlint

--- app/src/main/java/com/thinlineit/ctrlf/page/PageActivity.kt
+++ app/src/main/java/com/thinlineit/ctrlf/page/PageActivity.kt
@@ -20,7 +20,7 @@ class PageActivity : AppCompatActivity() {
             R.layout.activity_page
         )
     }
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val noteId = intent.getIntExtra(NOTE_ID, 0)
