e0aeaba1d1bba5cc0e2aa916108a97b2feb39534
Donggi Lee <rkdmf1026@naver.com>
2021-09-27 19:27:14 +0900

    Fix alternative layout
    변경된 디자인을 태블릿 레이아웃에도 적용

--- app/src/main/java/com/thinlineit/ctrlf/notes/NotesFragment.kt
+++ app/src/main/java/com/thinlineit/ctrlf/notes/NotesFragment.kt
@@ -3,12 +3,17 @@ package com.thinlineit.ctrlf.notes
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.thinlineit.ctrlf.R
 import com.thinlineit.ctrlf.databinding.FragmentNotesBinding
 
 class NotesFragment : Fragment() {
@@ -18,13 +23,13 @@ class NotesFragment : Fragment() {
             NotesFragmentDirections.actionNotesFragmentToPageActivity(noteId)
         )
     }
-    // TODO : 앱바 버튼 클릭 시 로그아웃액티비티로 전환하는 로직 구현
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        setHasOptionsMenu(true)
         val binding = FragmentNotesBinding.inflate(
             inflater,
             container,
@@ -32,13 +37,31 @@ class NotesFragment : Fragment() {
         ).apply {
             this.noteViewModel = this@NotesFragment.noteViewModel
             lifecycleOwner = this@NotesFragment
+            (requireActivity() as AppCompatActivity).setSupportActionBar(toolBar)
+            (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayShowTitleEnabled(
+                false
+            )
             noteListRecyclerView.adapter = noteAdapter
         }
+
         noteViewModel.alertLiveData.observe(viewLifecycleOwner) {
             // TODO: Check if response body is empty
             Toast.makeText(context, it, Toast.LENGTH_LONG).show()
             Log.e("loadNote Exception", it)
         }
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) =
+        inflater.inflate(R.menu.toolbar_main, menu)
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.userCircleBtn -> {
+            findNavController().navigate(
+                NotesFragmentDirections.actionNotesFragmentToLogoutActivity()
+            )
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
